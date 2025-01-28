121864b4e05fad4a8c864d15c64a37775949d670
revanza1106 <revanza.firdaus@gmail.com>
2025-01-28 13:09:53 +0700

    modified:CategoryController

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -6,15 +6,47 @@
 
 @section('content')
 
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Category</h1>
-        @if (session('message'))
-            <div class="alert alert-success">{{ session('message') }}</div>
-        @endif
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Category</li>
-        </ol>
-        <div class="row">
+    <div class="container-fluid px-4 p-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    View Category
+                    <a href={{ url('admin/add-category') }} class="btn btn-primary float-end">New category</a>
+                </h4>
+            </div>
+            <div class="card-body">
+                @if (session('message'))
+                    <div class="alert alert-success">{{ session('message') }}</div>
+                @endif
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Status</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($category as $data)
+                            <tr>
+                                <td>{{ $data->id }}</td>
+                                <td>{{ $data->name }}</td>
+                                <td>
+                                    <img src="{{ asset('uploads/category/' . $data->image) }}" width="50px" height="50px"
+                                        alt="img">
+                                </td>
+                                <td>{{ $data->status == '1' ? 'hidden' : 'shown' }}</td>
+                                <td>
+                                    <a href="{{ url('admin/edit-category') }}" class="btn btn-success">Edit</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
