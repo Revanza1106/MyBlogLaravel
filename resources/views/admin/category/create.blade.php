9c3d8993cb8943d16629267e8078c594e3642f78
revanza1106 <revanza.firdaus@gmail.com>
2025-01-28 10:37:34 +0700

    add:admin category:create

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -12,7 +12,16 @@
                 <h4>Add Category</h4>
             </div>
             <div class="card-body">
-                <form action="{{ url('admin/add-category') }}" method="POST">
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <div>{{ $error }}</div>
+                        @endforeach
+                    </div>
+                @endif
+                
+                <form action="{{ url('admin/add-category') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
                         <label for="">Category Name</label>
@@ -24,11 +33,11 @@
                     </div>
                     <div class="mb-3">
                         <label for="">Description</label>
-                        <textarea name="Description" class="form-control"></textarea>
+                        <textarea name="description" class="form-control"></textarea>
                     </div>
                     <div class="mb-3">
                         <label for="">Image</label>
-                        <input type="file" name="Image" class="form-control">
+                        <input type="file" name="image" class="form-control">
                     </div>
 
                     <h6>SEO Tags</h6>
@@ -49,14 +58,16 @@
                     <div class="row">
                         <div class="col-md-3 mb-3">
                             <label for="">Navbar Status</label>
-                            <input type="checkbox" name="navbar_status">
+                            <input type="hidden" name="navbar_status" value="0">
+                            <input type="checkbox" name="navbar_status" value="1">
                         </div>
                         <div class="col-md-3 mb-3">
                             <label for=""> Status</label>
-                            <input type="checkbox" name="status">
+                            <input type="hidden" name="status" value="0">
+            <input type="checkbox" name="status" value="1">
                         </div>
                         <div class="col-md-6">
-                            <div type="submit" class="btn btn-primary">Save Category</div>
+                            <button type="submit" class="btn btn-primary">Save Category</button>
                         </div>
                     </div>
                 </form>
