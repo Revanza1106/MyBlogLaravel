06a2a3c4aad4f63cc39c420ea2c281aa382aaf85
revanza1106 <revanza.firdaus@gmail.com>
2025-02-01 20:52:11 +0700

    add:post migration and model

--- database/migrations/2025_02_01_091630_create_post_table.php
+++ database/migrations/2025_02_01_091630_create_post_table.php
@@ -15,6 +15,17 @@ class CreatePostTable extends Migration
     {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->mediumText('description');
+
+            $table->string('yt_iframe');
+            $table->string('meta_title');
+            $table->mediumText('meta_description');
+            $table->mediumText('meta_keyword');
+            $table->tinyInteger('status');
+            $table->integer('created_by');
             $table->timestamps();
         });
     }
